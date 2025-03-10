2ad2fb4402e750c925212c119794d19de920d062
Claudio J Reinaldo <claudio.reinaldo@outlook.com>
2025-03-10 12:08:45 -0300

    Delete Palíndrono2.py

--- Prática 2/Prática 3/Palíndrono2.py
+++
@@ -1,9 +0,0 @@
-def verificar_palindromo():
-    frase = input("Digite uma frase: ")
-    frase = frase.replace(" ", "").lower()  # remove espaços e converte para minúsculas
-    if frase == frase[::-1]:
-        print("Sim")
-    else:
-        print("Não")
-
-verificar_palindromo()
